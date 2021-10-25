dc624aefc54a04ab5434ac216b1fc3fe504a12ed
iromero <iromero@factset.com>
2021-10-26 00:26:15 +0100

    add the Header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Shop from "./pages/shop/Shop";
+import Header from "./components/header/Header";
 
 import "./App.css";
 
 const App: React.FC = () => (
-  <Switch>
-    <Route exact path="/" component={HomePage} />
-    <Route path="/shop" component={Shop} />
-  </Switch>
+  <>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={Shop} />
+    </Switch>
+  </>
 );
 
 export default App;
