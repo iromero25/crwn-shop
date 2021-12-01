1d82929d66095d5abe51aac6c993510121664ac1
iromero <iromero@factset.com>
2021-12-01 22:08:37 +0000

    adding cart persistency for Redux

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -2,6 +2,14 @@ import { combineReducers } from "redux";
 import toggleCartReducer from "./cart/cart.reducer";
 import shopReducer from "./shop/shop.reducer";
 import userReducer from "./user/user.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persisConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"], // an array containig the key strings of the parts of the store that we want to persist
+};
 
 const rootReducer = combineReducers({
   user: userReducer,
@@ -11,4 +19,4 @@ const rootReducer = combineReducers({
 
 export type Store = ReturnType<typeof rootReducer>;
 
-export default rootReducer;
+export default persistReducer(persisConfig, rootReducer);
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
-import rootReducer from "./root-reducer";
 import createSagaMiddleware from "redux-saga";
+import { persistStore } from "redux-persist";
+import logger from "redux-logger";
+
+import rootReducer from "./root-reducer";
 import rootSaga from "./root-saga";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -11,4 +13,6 @@ const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
