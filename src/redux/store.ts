01f743342a32674621a9e313f98e769c21751eea
iromero <iromero@factset.com>
2022-01-02 02:39:24 +0000

    removing the logger from the production build

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, Middleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import logger from "redux-logger";
 
@@ -7,7 +7,12 @@ import rootSaga from "./root-saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [logger, sagaMiddleware];
+const middlewares: Middleware<any>[] = [sagaMiddleware];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
+
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 sagaMiddleware.run(rootSaga);
