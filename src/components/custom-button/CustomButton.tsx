10d9711bd69e96b0b5022d308b2cda376d63ace7
iromero <iromero@factset.com>
2022-02-13 00:48:52 +0000

    fix a styling bug on the CustomButton component

--- src/components/custom-button/CustomButton.tsx
+++ src/components/custom-button/CustomButton.tsx
@@ -25,11 +25,19 @@ const CustomButton: React.FunctionComponent<Props> = ({
   // is to control the  backgroundColor  style of  my button  programatically
   // (instead  of  relying on the css styling).  This is  the only way I know
   // to test that a style is present in the button (see the related test)
+
+  // update: when we add cypress we might be able to revert this  back to  the
+  // way it is as cypress should allow us to test the styling in a differently
+  // I don't like having to control every single part of the styling like this
+  // so it can be tested
   return (
     <button
       onMouseEnter={() => setBackground("white")}
       onMouseLeave={() => setBackground("black")}
-      style={{ backgroundColor: `${background}` }}
+      style={{
+        backgroundColor: `${background}`,
+        color: `${background === "black" ? "white" : "black"}`,
+      }}
       className={`${inverted ? "inverted" : ""} ${
         isGoogleSignIn ? "google-sign-in" : ""
       } custom-button`}
