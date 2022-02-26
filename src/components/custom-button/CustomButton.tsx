dc912f4933393e249a62bcdd3119c6ea1ffc54b0
iromero25 <72737757+iromero25@users.noreply.github.com>
2022-02-26 23:29:55 +0000

    CustomButton: remove unnecessary code; style testing is handled by cypress (#13)
    Testing the Custom Button style is better handled by cypress: checking how a component looks in the browser is cypress' motive and thus we test this over there.
    
    The way the testing-library was executing this test was by literally checking if there's a style applied to the button; remember that testing-library relies on the creation of a virtual DOM tree and thus, no actual styling is applied. Cypress can actually check how the button looks to the user since it is directly accessing the DOM generated in the browser.

--- src/components/custom-button/CustomButton.tsx
+++ src/components/custom-button/CustomButton.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-
+import React from "react";
 import firebase from "firebase/compat";
 
 import "./custom-button.scss";
@@ -18,34 +17,18 @@ const CustomButton: React.FunctionComponent<Props> = ({
   isGoogleSignIn,
   inverted,
   ...otherProps
-}) => {
-  const [background, setBackground] = useState<string>("black");
-
-  // the only reasson I add functionality for the `onMouseEnter/Leave` events
-  // is to control the  backgroundColor  style of  my button  programatically
-  // (instead  of  relying on the css styling).  This is  the only way I know
-  // to test that a style is present in the button (see the related test)
-
-  // update: when we add cypress we might be able to revert this  back to  the
-  // way it is as cypress should allow us to test the styling in a differently
-  // I don't like having to control every single part of the styling like this
-  // so it can be tested
-  return (
-    <button
-      onMouseEnter={() => setBackground("white")}
-      onMouseLeave={() => setBackground("black")}
-      style={{
-        backgroundColor: `${background}`,
-        color: `${background === "black" ? "white" : "black"}`,
-      }}
-      className={`${inverted ? "inverted" : ""} ${
-        isGoogleSignIn ? "google-sign-in" : ""
-      } custom-button`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
-};
+}) => (
+  // testing how this botton looks in the browser is handled by cypress:
+  // cypress can actually check how the button looks  to the  user  (and
+  // thus check the applied colors)
+  <button
+    className={`${inverted ? "inverted" : ""} ${
+      isGoogleSignIn ? "google-sign-in" : ""
+    } custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
+);
 
 export default CustomButton;
